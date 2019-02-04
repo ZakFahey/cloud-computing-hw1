a22ba4739b43d58e142aee26350b9761429d86ef
ZakFahey <z.fahey@gmail.com>
2019-02-04 17:23:40 -0500

    Finished response handling

--- static/scripts.js
+++ static/scripts.js
@@ -16,5 +16,35 @@ setInterval(rotatePlaceholder, 2000);
 
 function checkWebsite() {
     var url = document.getElementById("sitename").value;
-    alert();
+
+    document.getElementById('no').style.display = 'none';
+    document.getElementById('yes').style.display = 'none';
+
+    var request = new XMLHttpRequest();
+    request.open('GET', '/api/status?url=' + encodeURI(url), true);
+
+    request.onload = function () {
+        var success = JSON.parse(this.response).success;
+        if (success) {
+            // Website is not down
+            document.getElementById('no').style.display = 'block';
+            document.getElementById('yes').style.display = 'none';
+            document.getElementById('name-no').innerText = document.getElementById("sitename").value;
+        } else {
+            // Website is down
+            document.getElementById('yes').style.display = 'block';
+            document.getElementById('no').style.display = 'none';
+            document.getElementById('name-yes').innerText = document.getElementById("sitename").value;
+        }
+    };
+
+    request.onerror = function () {
+        alert('Request failed to send.');
+    };
+
+    request.send();
+}
+
+function disableButton(textbox) {
+    document.getElementById('submitButton').disabled = (textbox.value.length === 0);
 }
